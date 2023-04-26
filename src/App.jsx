57a2a693015ab514891a4c22cb8979aa109502eb
George Balch <george.balch@proton.me>
2023-04-26 11:38:21 -0700

    Add handler to set current file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
 
   return (
     <div>
-      <input type="file" name="media" />
+      <input 
+        type="file" 
+        name="media" 
+        onChange={changeHandler} />
+      <button>Generate Subtitles</button>
     </div>
   )
 }
