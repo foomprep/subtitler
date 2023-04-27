69ddd2843730d4aa9bf3b3d1a2fa43e6c47a8fb3
George Balch <george.balch@proton.me>
2023-04-26 22:44:39 -0700

    Check for subtitles based on video current time and timestamps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Subtitles from './components/Subtitles'
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
   const [videoURL, setVideoURL] = useState();
-  const [subtitles, setSubtitles] = useState('');
+  const [displayedSubtitle, setDisplayedSubtitle] = useState('');
+  const [subtitles, setSubtitles] = useState({});
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
   }
 
   const handleUpload = async () => {
+    setDisplayedSubtitle('');
     setVideoURL(URL.createObjectURL(selectedFile));
     const formData = new FormData();
     formData.append('file', selectedFile);
     const response = await axios.post('http://localhost:5001/transcribe', formData)
-    setSubtitles(response.data.text);
+    setSubtitles(response.data);
   }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const video = document.getElementById('video');
+      const currentTime = video.currentTime;
+      for (let i=0; i<subtitles.start.length; i++) {
+        if (currentTime > subtitles.start[i] && currentTime < subtitles.end[i]) {
+          setDisplayedSubtitle(subtitles.text[i]);
+          break;
+        }
+      }
+    }, 100)
+
+    return () => clearInterval(interval);
+  }, [subtitles])
 
   return (
     <div>
-      <video height="400" src={videoURL} controls />
+      <video id='video' height="400" src={videoURL} controls />
       <input 
         type="file" 
         name="media" 
         onChange={changeHandler} />
       <button onClick={handleUpload}>Generate Subtitles</button>
-      <Subtitles subtitles={subtitles} />
+      <Subtitles subtitles={displayedSubtitle} />
     </div>
   )
 }
