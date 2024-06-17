ce7604fb5783c01279ef1f2175beb2e1715c96f0
George Balch <george.balch@proton.me>
2024-06-16 20:29:44 -0700

    Trim word before translating

--- src/components/Word/index.jsx
+++ src/components/Word/index.jsx
@@ -4,8 +4,12 @@ import './Word.css';
 const Word = ({
     word
 }) => {
+    const trimWord = (word) => {
+        return word.replace('.', '').replace(',', '').replace('?', '').replace('!', '').replace(';', '').replace(':', '').replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace('<', '').replace('>', '').replace('"', '').replace("'", '');
+    }
 
     const handleClick = async () => {
+        const trimmed = trimWord(word);
         const response = await axios.post('http://localhost:5001/translate', {
             text: word
         })
