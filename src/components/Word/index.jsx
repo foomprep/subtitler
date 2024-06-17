3a75275dc51b1933b8a95cdf32c035757e03fa7e
George Balch <george.balch@proton.me>
2024-06-16 19:22:09 -0700

    On word click generate translation and print to console

--- src/components/Word/index.jsx
+++ src/components/Word/index.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import './Word.css';
 
 const Word = ({
@@ -5,6 +6,10 @@ const Word = ({
 }) => {
 
     const handleClick = async () => {
+        const response = await axios.post('http://localhost:5001/translate', {
+            text: word
+        })
+        console.log(response.data['translation_text']);
     }
 
     return (
